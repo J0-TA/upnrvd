74e6e37593940437dac8f520b562daece2443f8f
Juan Carlos <jcarlosriego@gmail.com>
2022-03-03 23:54:57 +0100

    Deleted index.css, applied styles and basic logic to MovieCard

--- src/features/Catalog/Catalog.jsx
+++ src/features/Catalog/Catalog.jsx
@@ -7,10 +7,16 @@ import MovieCard from '../MovieCard/MovieCard';
 
 const Catalog = () => {
     return (
-            <Grid container justifyContent='space-around'>
-                {moviesDB.map((movie, idx) => <MovieCard movie={movie} idx={idx + 1} key={`${movie.title}${movie.year}`}/>)}
+            <Grid container display='flex' flexDirection='column' alignItems='center' justifyContent='space-around'>
+                {moviesDB.map((movie, idx) => (
+                    <Grid item margin={4}>                   
+                        <MovieCard movie={movie} idx={idx + 1} key={`${movie.title}${movie.year}`}/>
+                    </Grid>))
+                }                    
             </Grid>
     )
 };
 
 export default Catalog;
+
+
--- src/features/MovieCard/MovieCard.jsx
+++ src/features/MovieCard/MovieCard.jsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import { Badge, Card, CardActions, CardContent, CardMedia, Chip, IconButton, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Badge, Card, CardActions, CardContent, CardMedia, Chip, Collapse, IconButton, Typography } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+
 const MovieCard = ({ movie, idx }) => {
     const { title, year, director, duration, genre, rate } = movie;
-    
+    const [ expanded, setExpanded ] = useState(false);
+
+    const handleExpandClick = () => setExpanded(!expanded);
+    const handleEditClick = () => window.alert('Edit'); 
+    const handleDeleteClick = () => window.alert('Delete');
+        
     return (
-        <Badge badgeContent={rate} color="secondary">
-            <Card sx={{ width: 275, height: 500}}>
+        <Badge badgeContent={rate} color="success">
+            <Card sx={{ width: 450, cursor:'pointer'}} onClick={handleExpandClick}>              
                 <CardMedia 
                     component='img'
-                    height='200'
-                    image={`https://picsum.photos/200/300?random=${idx}`}
+                    height='250'
+                    image={`https://picsum.photos/450?random=${idx}`}
                     alt='movie poster fake'
                 />
                 <CardContent>
                     <Typography variant="h5" component="div" color="primary">
                         {title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {`${director} (${year})`}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Duration: {duration}
-                    </Typography>                    
-                    {genre.map((genre, idx) => <Chip size="small" label={genre} key={`${genre}${idx}`} color='secondary' />)}
                 </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="edit movie">
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton aria-label="delete movie">
-                        <DeleteIcon />
-                    </IconButton>
-                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            {`${director} (${year}). ${duration}`}
+                        </Typography>                        
+                        {genre.map((genre, idx) => (
+                            <Chip 
+                                sx={{m: 1}} 
+                                size="small" 
+                                label={genre} 
+                                key={`${genre}${idx}`} 
+                                color='secondary' 
+                            />))}
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="edit movie" onClick={handleEditClick}>
+                                <EditIcon />
+                            </IconButton>
+                            <IconButton aria-label="delete movie" onClick={handleDeleteClick}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </CardActions>
+                    </CardContent>                 
+                </Collapse>
             </Card>
         </Badge>
     ) 
